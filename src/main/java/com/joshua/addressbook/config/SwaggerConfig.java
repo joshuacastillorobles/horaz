e58aa388be869762e9160b8b4816a34fa44691eb
Horacio Conde <hconde@anzen.com.mx>
2018-12-29 19:23:17 -0600

    Swagger configuration updated for api package

--- src/main/java/com/joshua/addressbook/config/SwaggerConfig.java
+++ src/main/java/com/joshua/addressbook/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.joshua.addressbook"))
+				.apis(RequestHandlerSelectors.basePackage("com.joshua.addressbook.api"))
 				.build()
 				.apiInfo(apiInfo());
 	}
